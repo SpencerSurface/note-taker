c30886180be98c1e0ba951e81c491bb4584596b1
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-02-02 16:39:38 -0500

    add code to serve the HTML files

---
+++ server.js
@@ -0,0 +1,31 @@
+// Import statements
+const express = require("express");
+const path = require("path");
+const api = require("./routes/index");
+
+// Define port number
+const PORT = process.env.PORT || 3001;
+
+// Initialize Express app
+const app = express();
+
+// Use middleware to parse JSON
+app.use(express.json());
+
+// Use /public to serve static files
+app.use(express.static("public"));
+
+// GET route for homepage
+app.get("/", (req, res) => 
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+// GET route for notes page
+app.get("/notes", (req, res) => 
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+// Bind app to port and start listening
+app.listen(PORT, () => 
+    console.log(`App listening on port http://localhost:${PORT}`)
+);
