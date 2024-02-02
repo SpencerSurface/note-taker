3b8d548e1fe65aaadc315ceb886829b30ee53d9c
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-02-02 17:27:54 -0500

    update readFile to use promises, add an encoding for readFile

--- routes/notes.js
+++ routes/notes.js
@@ -1,19 +1,20 @@
 // Import statements
 const notes = require("express").Router();
-const {readFile} = require("fs");
+const {readFile, writeFile} = require("fs/promises");
 
-// Add GET route for sending all the notes
+// Add GET route for requesting to view all the notes
 notes.get("/", (req, res) => {
     // Read the notes from the file
-    readFile("./db/db.json", (err, data) => {
+    readFile("./db/db.json", "utf-8").then((data, err) => {
         if (err) {
             // If there's an error, log an error message
+            console.error(err);
             console.error("Error reading notes from file");
         } else {
             // Else, respond to the request with the notes
             res.json(JSON.parse(data));
         }
-    })
+    });
 });
 
 module.exports = notes;
